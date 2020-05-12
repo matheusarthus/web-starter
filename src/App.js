e7641643dcd51751cb22e880ab42aa6e32de7e7e
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-05-12 14:45:29 -0300

    chore: initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/* import Routes from './routes';
+import GlobalStyle from './styles/global'; */
+
+function App() {
+  return <h1>TESTE</h1>;
+}
+
+export default App;
